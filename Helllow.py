4fff19bbb8a00be43891908ac13a3ce04a290080
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-02-27 12:49:31 +0600

    done Creat a obj and more

--- Helllow.py
+++ Helllow.py
@@ -6,15 +6,21 @@ class Animal:
         self.quality = behave
 
     def set_name(self, new_name):
+        if (self.__hash_any_number(new_name)):
+            print("Sorry Person name can't have any number")
+            return
         self.name = new_name
+
+    def __hash_any_number(self, string):
+        return "0" in string
 
     def get_details(self):
         return f"Animal: {self.animal},name :{self.name},Color :{self.color}, Behave: {self.quality} "
 
 
 one_animal = Animal("cat", "White_ninja", "white", "angry")
-
 print(one_animal.get_details())
-
 one_animal.set_name("white x")
+print(one_animal.get_details())
+one_animal.set_name("0whitex")
 print(one_animal.get_details())
