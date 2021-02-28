0450558c0c8250a0da09bdfa483ede4e97fc8a88
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-02-28 21:07:29 +0600

    Data Struckture Double Arraylist

--- Helllow.py
+++ Helllow.py
@@ -1,68 +1,38 @@
-class Person:
-    def __init__(self, name: str, age: int, born: int):
-        self.__name = name
-        self.__age = age
-        self.__date_of_birh = born
-
-    def name(self):
-        return self.__name
-
-    def get_of_birth(self):
-        return self.__date_of_birh
-
-    def set_new_name(self, new_name):
-        if __has_any_number(new_name):
-            print("plz enter only text")
-            return
-        self.__name = new_name
-
-    def get_details(self):
-        return f"Name :{self.__name}\nAge  :{self.__age}\nBorn :{self.__date_of_birh}"
-
-    def __has_any_number(self, string):
-        return "0" in string
+class Node:
+    def __init__(self, vaule):
+        self.next = None
+        self.prev = None
+        self.vaule = vaule
 
 
-person_list = [(Person("Mehadi", 17, 2004)),
-               Person("Captain jack", 60, 1989),
-               Person("Stevs jobs", 60, 1960),
-               Person("Rhaan paul", 30, 1990)
-               ]
-# for person in person_list:
-#    if person.get_of_birth() >= 1960:
-#        print(person.get_details())
+class DuoLinkListed:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
 
-
-# Creat a new class :
-
-class Student(Person):
-    def __init__(self, name, age, born, phone_number: int, students_id: str):
-        super().__init__(name, age, born)
-        self.phone = phone_number
-        self.student_id = students_id
-
-    def get_details(self):
-        return f"Name :{self.name()} \n Born{self.get_of_birth()}\nNo :{self.phone}"
+    def add(self, vaule):
+        node = Node(vaule)
+        if self.tail is None:
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            self.head.next=node
+            node.prev=self.head
+            self.tail=node
+            self.size+=2
 
     def __str__(self):
-        return f"Name :{self.name()} \n Born{self.get_of_birth()}\nNo :{self.phone}"
+        value=[]
+        Node=self.head
+        while node is not None:
+            value.append(node.vaule)
+            node=node.next
+        return 
 
-
-student_one = Student("mehadi", 17, 2004, 1922571100, "GSCA-20210022")
-print(student_one)
-
-
-class Teacher(Person):
-    def __init__(self, name, age, born, deparment: str):
-        super().__init__(name, age, born)
-        self.dep = deparment
-
-
-new_person_list = [
-    Person("mehadi", 17, 2020),
-    Student("S", 17, 2000, 1922571100, "Gsca -11"),
-    Teacher("s", 50, 1990, "CSE")
-]
-
-for mix in new_person_list:
-    print(mix.get_details())
+my_list=DuoLinkListed()
+my_list.add(1)
+my_list.add(6)
+my_list.add(2)
+print(my_list)
