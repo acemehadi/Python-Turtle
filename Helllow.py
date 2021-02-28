527c563a5005106c44beb7a6244a00651f9327be
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-02-28 10:50:21 +0600

    Complete object oriend Programming

--- Helllow.py
+++ Helllow.py
@@ -1,26 +1,68 @@
-class Animal:
-    def __init__(self, animal, name, color, behave):
-        self.animal = animal
-        self.name = name
-        self.color = color
-        self.quality = behave
+class Person:
+    def __init__(self, name: str, age: int, born: int):
+        self.__name = name
+        self.__age = age
+        self.__date_of_birh = born
 
-    def set_name(self, new_name):
-        if (self.__hash_any_number(new_name)):
-            print("Sorry Person name can't have any number")
+    def name(self):
+        return self.__name
+
+    def get_of_birth(self):
+        return self.__date_of_birh
+
+    def set_new_name(self, new_name):
+        if __has_any_number(new_name):
+            print("plz enter only text")
             return
-        self.name = new_name
-
-    def __hash_any_number(self, string):
-        return "0" in string
+        self.__name = new_name
 
     def get_details(self):
-        return f"Animal: {self.animal},name :{self.name},Color :{self.color}, Behave: {self.quality} "
+        return f"Name :{self.__name}\nAge  :{self.__age}\nBorn :{self.__date_of_birh}"
+
+    def __has_any_number(self, string):
+        return "0" in string
 
 
-one_animal = Animal("cat", "White_ninja", "white", "angry")
-print(one_animal.get_details())
-one_animal.set_name("white x")
-print(one_animal.get_details())
-one_animal.set_name("0whitex")
-print(one_animal.get_details())
+person_list = [(Person("Mehadi", 17, 2004)),
+               Person("Captain jack", 60, 1989),
+               Person("Stevs jobs", 60, 1960),
+               Person("Rhaan paul", 30, 1990)
+               ]
+# for person in person_list:
+#    if person.get_of_birth() >= 1960:
+#        print(person.get_details())
+
+
+# Creat a new class :
+
+class Student(Person):
+    def __init__(self, name, age, born, phone_number: int, students_id: str):
+        super().__init__(name, age, born)
+        self.phone = phone_number
+        self.student_id = students_id
+
+    def get_details(self):
+        return f"Name :{self.name()} \n Born{self.get_of_birth()}\nNo :{self.phone}"
+
+    def __str__(self):
+        return f"Name :{self.name()} \n Born{self.get_of_birth()}\nNo :{self.phone}"
+
+
+student_one = Student("mehadi", 17, 2004, 1922571100, "GSCA-20210022")
+print(student_one)
+
+
+class Teacher(Person):
+    def __init__(self, name, age, born, deparment: str):
+        super().__init__(name, age, born)
+        self.dep = deparment
+
+
+new_person_list = [
+    Person("mehadi", 17, 2020),
+    Student("S", 17, 2000, 1922571100, "Gsca -11"),
+    Teacher("s", 50, 1990, "CSE")
+]
+
+for mix in new_person_list:
+    print(mix.get_details())
