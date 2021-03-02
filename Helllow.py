ee7ba63a9df4bc6a6cfeeec04ef97efa7c6cec9b
mehadihasan00 <meahdihasan.rajib10@gmail.com>
2021-03-02 11:57:21 +0600

    credit card

--- Helllow.py
+++ Helllow.py
@@ -1,38 +1,6 @@
-class Node:
-    def __init__(self, vaule):
-        self.next = None
-        self.prev = None
-        self.vaule = vaule
+t=(1,2,3)
+credits_card_one=(12345678,"mehadi",10/2022,568)
+credits_card_two=(12345678,"White x,",10/2023,979)
 
-
-class DuoLinkListed:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
-
-    def add(self, vaule):
-        node = Node(vaule)
-        if self.tail is None:
-            self.head = node
-            self.tail = node
-            self.size += 1
-        else:
-            self.head.next=node
-            node.prev=self.head
-            self.tail=node
-            self.size+=2
-
-    def __str__(self):
-        value=[]
-        Node=self.head
-        while node is not None:
-            value.append(node.vaule)
-            node=node.next
-        return 
-
-my_list=DuoLinkListed()
-my_list.add(1)
-my_list.add(6)
-my_list.add(2)
-print(my_list)
+credit_card=[credits_card_one,credits_card_two]
+print(credit_card)
